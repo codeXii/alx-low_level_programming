e78fcdd6f183308e68c1b2fb8f473fba7b46a126
codeXii <iwekimorn@gmail.com>
2022-12-13 04:40:10 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * 'prints the lower case alphabets 10x'
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < = 9; i++)
+	{
+		int j = 'a';
+
+		while (j = 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+	putchar('\n');
+	return (0);
+}
