6e6c43658d86cf9486d0177dc2d3c4b50fe06398
codeXii <iwekimorn@gmail.com>
2022-12-13 03:23:59 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "Main.h"
+
+/**
+ * main - Entry block
+ * 'prints _putchar'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+
+	return (0);
+}
