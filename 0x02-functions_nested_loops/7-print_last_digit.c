65c98edadde28e91ecc9fcd53ebabae9ba87798b
codeXii <iwekimorn@gmail.com>
2022-12-13 06:13:43 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - main entry point
+ * @n: 'last value of the interger is printed
+ * Return: 0 success
+ */
+
+int print_last_digit(int n)
+{
+	int last_d = n % 10;
+
+	if (last_d < 0)
+		last_d *= -1;
+
+	putchar(last_d + '0');
+	return (last_d);
+}
+
