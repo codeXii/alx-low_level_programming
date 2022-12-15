ce26785ec5429ebf1d810657bcb099457da6ff79
CodeXii <78667460+codeXii@users.noreply.github.com>
2022-12-15 11:23:44 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: The number to be checked.
+ * @b: The input to be multiplied
+ * Return: the sum of the two integers.
+ *
+ */
+
+int mul(int a, int b);
+{
+      sum = a * b;
+  
+     return (sum);
+}
