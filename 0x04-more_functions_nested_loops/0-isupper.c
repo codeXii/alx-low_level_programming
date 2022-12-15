d030a963569d31b28962175873bdeacb4d55b57e
CodeXii <78667460+codeXii@users.noreply.github.com>
2022-12-15 11:12:00 -0800

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,8 +9,12 @@
 
 int _isupper(int c)
 {
-  if (C >= 'A' && c <= 'Z')
-        return (1);
-  else
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
