eab8e126b35bb980cc61959478a3578c075ad768
CodeXii <78667460+codeXii@users.noreply.github.com>
2022-12-15 11:07:31 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - main entry point
+*@c: input character
+*Return: 1 if its an upper case other wise 0
+*/
+
+int _isupper(int c)
+{
+  if (C >= 'A' || c <= 'Z')
+        return (1);
+  else
+        return (0);
+}
