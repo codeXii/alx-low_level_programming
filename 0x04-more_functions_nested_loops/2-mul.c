5c151157ffdfa65ac3b8285712fe51803a278c73
CodeXii <78667460+codeXii@users.noreply.github.com>
2022-12-15 11:26:07 -0800

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,16 +2,13 @@
 
 /**
  * mul - multiplies two integers
- *
  * @a: The number to be checked.
  * @b: The input to be multiplied
  * Return: the sum of the two integers.
- *
  */
 
 int mul(int a, int b);
 {
       sum = a * b;
-  
-     return (sum);
+      return (sum);
 }
