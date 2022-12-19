c6eeb1e59b48908f1eccd9bfb58fd8dfa6638e18
codeXii <iwekimorn@gmail.com>
2022-12-19 14:43:03 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: input string
+ * Return: lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*(s + 1))
+		lenght++;
+	return (lenght);
+}
