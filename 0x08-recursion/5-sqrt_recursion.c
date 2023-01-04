44ebb44ad4f1eebd22ed190308f1c9a8f36e367a
CodeXii <78667460+codeXii@users.noreply.github.com>
2023-01-04 14:41:40 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _sqrt_recursion - outputs squareroot of n
+ *
+ * @n: value
+ * Return: void
+ */
+
+int _sqrt_recursion(int n)
+{
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	n++;
+		return (_sqrt_recursion(n));
+}
