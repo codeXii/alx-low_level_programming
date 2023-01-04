791293b746090ecffa1d841c3242a247c3441894
CodeXii <78667460+codeXii@users.noreply.github.com>
2023-01-04 14:39:33 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _pow_recursion - outputs the result of x raised to y
+ *
+ * @x: value
+ * @y: value
+ * Return: void
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (x == 0)
+	{
+		return (0);
+	}
+	if (x == 1 || y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
