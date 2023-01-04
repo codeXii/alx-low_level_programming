b1cbd6203a8487a5c09bcff1a2b44bc8e48aaa94
CodeXii <78667460+codeXii@users.noreply.github.com>
2023-01-04 14:52:05 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,48 +1,44 @@
 #include "main.h"
 
 /**
- * _strlen - get string length
+ * _strlen_recursion - returns the length of a string.
  * @s: string
- * Return: Length
+ * Return: the length of a string.
  */
-
-int _strlen(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s == END)
-		return (NULL);
-	s++;
-	return (1 + _strlen(s));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * split_n_mirror - compare 1st half to the mirror of 2nd half
- * @s_head: 1st half of string
- * @s_tail: 2nd half of string
- *
- * Return: SUCCESS ? 1 : 0
+ * compare_to - compares each character of the string.
+ * @s: string
+ * @a: smallest iterator.
+ * @b: biggest iterator.
+ * Return: .
  */
-
-int split_n_mirror(char *s_head, char *s_tail)
+int compare_to(char *s, int a, int b)
 {
-	if (s_head >= s_tail)
-		return (TRUE);
-
-	if (*s_head == *s_tail)
-		return (split_n_mirror(s_head + 1, s_tail - 1));
-
-	return (NULL);
+	if (*(s + a) == *(s + b))
+	{
+		if (a == b || a == b + 1)
+			return (1);
+		return (0 + compare_to(s, a + 1, b - 1));
+	}
+	return (0);
 }
 
 /**
- * is_palindrome - check if a string is palindrome
- * @s: string
- *
- * Return: SUCCESS ? 1 : 0
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
  */
-
 int is_palindrome(char *s)
 {
-	int len = _strlen(s);
-
-	return (split_n_mirror(s, s + len - 1));
+	if (*s == '\0')
+		return (1);
+	return (compare_to(s, 0, _strlen_recursion(s) - 1));
 }
